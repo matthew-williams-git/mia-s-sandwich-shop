77098e2ca349598146cb26b775dd23ccc48de373
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-08 16:09:59 +0000

    added many files

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Mia's Sandwich Shop</title>
+        <link rel="stylesheet" href="universal.css">
+    </head>
+    <body>
+        <!-- very top header -->
+        <?php
+            include('top_bar.php')
+        ?>
+        <!-- navigation bar -->
+        <!-- getting started page -->
+        <!-- footer -->
+    </body>
+</html>
---
+++ top_bar.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="top_bar.css">
+    </head>
+
+    <!-- search bar, top left -->
+    <div class="search_bar">
+        <div class="search_icon">
+            <img src="Search Icon.png" alt="Search">
+        </div>
+        <p>Search all items</p>
+        <div class="plus_icon">
+            <img src="Plus Icon.png" alt="Add more">
+        </div>
+    </div>
+    
+    <!-- center brand name -->
+    <div class="mias_ss">
+        <p>Mia's Sandwich Shop</p>
+    </div>
+
+    <!-- developer mode, slightly right of main title -->
+    <div class="dev_mode">
+        <div class="toggle">
+            <img src="On Off Icon.png" alt="Developer mode on">
+        </div>
+        <p>Developer Mode</p>
+    </div>
+
+    <!-- account -->
+    <div class="account">
+        <div class="account_icon">
+            <img src="User Icon.png" alt="Account">
+        </div>
+        <p>Nick Bosley</p>
+        <div class="dropdown_icon">
+            <img src="DropDown Icon.png" alt="Drop down">
+        </div>
+    </div>
+
+    <!-- help button, right hand side -->
+    <div class="help_btn">
+        <p>Help</p>
+    </div>
+</html>
