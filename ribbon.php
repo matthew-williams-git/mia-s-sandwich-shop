5b7a06c367303239b9d671da83556fdc8ab9fd06
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-14 15:23:14 +0000

    I live the flowers

--- ribbon.php
+++ ribbon.php
@@ -27,3 +27,32 @@
         <p>POS</p>
     </div>
     
+    <div class="calender_tab">
+        <img src="Caldender Logo.png" alt="Calender" style="width:50px;height:50px">
+        <p>Calender</p>
+    </div>
+
+    <div class="reports_tab">
+        <img src="Reports Logo.png" alt="Reports" style="width:50px;height:50px">
+        <p>Reports</p>
+    </div>
+
+    <div class="delivery_tab">
+        <img src="Delivery Logo.png" alt="Deliveries" style="width:50px;height:50px">
+        <p>Delivery</p>
+    </div>
+
+    <div class="product_tab">
+        <img src="18 1.png" alt="Products" style="width:50px;height:50px">
+        <p>Prroducts</p>
+    </div>
+
+    <div class="admin_tab">
+        <img src="Admin Logo.png" alt="Administration" style="width:50px;height:50px">
+        <p>Admin</p>
+    </div>
+
+    <div class="more_tab">
+        <img src="More Logo.png" alt="More" style="width:50px;height:50px">
+        <p>More</p>
+    </div>
