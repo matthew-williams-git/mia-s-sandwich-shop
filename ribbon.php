730129b23fb85d53e7e39b6bb0eb91a2cd36458b
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-14 13:35:41 +0000

    added icon to change colour

--- ribbon.php
+++ ribbon.php
@@ -3,16 +3,16 @@
 </head>
 <body>
     <div class="customer_tab">
-        <img src="User Icon.png" alt="Customers" style="width:50px;height:50px">
+        <img src="User Icons.png" alt="Customers" style="width:50px;height:50px">
         <p>Customers</p>
     </div>
 
     <div class="order_tab">
-        <img src="Ticket Logo.png" alt="Orders">
+        <img src="Ticket Logo.png" alt="Orders" style="width:50px;height:50px">
         <p>Orders</p>
     </div>
 
     <div class="invoice_tab">
-        <img src="Invoice Logo.png" alt="Invoices">
+        <img src="Invoice Logo.png" alt="Invoices" style="width:50px;height:50px">
         <p>Invoices</p>
     </div>
