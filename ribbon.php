d0be15bb2971922c90cc3e8cfd8f1c93246d5f59
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-14 11:08:13 +0000

    adding stuff and making ribon

--- ribbon.php
+++ ribbon.php
@@ -2,4 +2,17 @@
     <link rel="stylesheet" href="ribbon.css">
 </head>
 <body>
-    <h1>this works</h1>
+    <div class="customer_tab">
+        <img src="User Icon.png" alt="Customers" style="width:50px;height:50px">
+        <p>Customers</p>
+    </div>
+
+    <div class="order_tab">
+        <img src="Ticket Logo.png" alt="Orders">
+        <p>Orders</p>
+    </div>
+
+    <div class="invoice_tab">
+        <img src="Invoice Logo.png" alt="Invoices">
+        <p>Invoices</p>
+    </div>
