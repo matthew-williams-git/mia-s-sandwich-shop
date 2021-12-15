ba6d3436ac1e44c158f555da7448adcb8b3b3784
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-15 14:17:43 +0000

    adding shnazzle

--- ribbon.php
+++ ribbon.php
@@ -10,70 +10,70 @@
         </div>
     </a>
 
-    <a href="#order">
+    <a href="order_page.php">
         <div class="order_tab">
             <img src="ribbon_images/Ticket Logo.png" alt="Orders" style="width:50px;height:50px">
             <p>Orders</p>
         </div>
     </a>
 
-    <a href="#invoices">
+    <a href="invoice_page1.php">
         <div class="invoice_tab">
             <img src="ribbon_images/Invoice Logo.png" alt="Invoices" style="width:50px;height:50px">
             <p>Invoices</p>
         </div>
     </a>
 
-    <a href="#inventory">
+    <a href="inventory_page.php">
     <div class="inventory_tab">
         <img src="ribbon_images/Inventory Logo.png" alt="Inventory" style="width:50px;height:50px">
         <p>Inventory</p>
     </div>
     </a>
 
-    <a href="#pos">
+    <a href="pos_page.php">
     <div class="pos_tab">
         <img src="ribbon_images/POS LOgo.png" alt="Point of sales" style="width:50px;height:50px">
         <p>POS</p>
     </div>
     </a>
     
-    <a href="#calender">
+    <a href="calender_page.php">
     <div class="calender_tab">
         <img src="ribbon_images/Caldender Logo.png" alt="Calender" style="width:50px;height:50px">
         <p>Calender</p>
     </div>
     </a>
 
-    <a href="#reports">
+    <a href="reports_page.php">
     <div class="reports_tab">
         <img src="ribbon_images/Reports Logo.png" alt="Reports" style="width:50px;height:50px">
         <p>Reports</p>
     </div>
     </a>
 
-    <a href="#delivery">
+    <a href="delivery_page.php">
     <div class="delivery_tab">
         <img src="ribbon_images/Delivery Logo.png" alt="Deliveries" style="width:50px;height:50px">
         <p>Delivery</p>
     </div>
     </a>
 
-    <a href="#products">
+    <a href="products_page.php">
     <div class="product_tab">
         <img src="ribbon_images/18 1.png" alt="Products" style="width:50px;height:50px">
-        <p>Prroducts</p>
+        <p>Products</p>
     </div>
     </a>
 
-    <a href="#admin">
+    <a href="admin_page1.php">
     <div class="admin_tab">
         <img src="ribbon_images/Admin Logo.png" alt="Administration" style="width:50px;height:50px">
         <p>Admin</p>
     </div>
     </a>
 
-    <a href="#more">
+    <a href="more_page.php">
     <div class="more_tab">
         <img src="ribbon_images/More Logo.png" alt="More" style="width:50px;height:50px">
         <p>More</p>
