f8c46bd2242b74f85123fcb9d3ef4714710b91e6
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-15 12:47:03 +0000

    opsy

--- im-imaginative.php
+++ im-imaginative.php
@@ -1,5 +1,7 @@
 <?php
-    include('top_bar.php')
-    include('ribbon.php')
+    include('top_bar.php');
+    include('ribbon.php');
 ?>
-<h1>In the jungle, the mighty jungle, the lion sleeps tonight</h1>
+<body>
+    <h1>In the jungle, the mighty jungle, the lion sleeps tonight</h1>
+</body>
