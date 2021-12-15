b74860e4fadcba68fa993200fa8fa4693870b536
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-15 11:42:03 +0000

    change div tags to button tag

--- ribbon.php
+++ ribbon.php
@@ -6,57 +6,57 @@
     <button class="customer_tab">
         <img src="User Icons.png" alt="Customers" style="width:50px;height:50px">
         <p>Customers</p>
-</button>
+    </button>
 
-    <div class="order_tab">
+    <button class="order_tab">
         <img src="Ticket Logo.png" alt="Orders" style="width:50px;height:50px">
         <p>Orders</p>
-    </div>
+    </button>
 
-    <div class="invoice_tab">
+    <button class="invoice_tab">
         <img src="Invoice Logo.png" alt="Invoices" style="width:50px;height:50px">
         <p>Invoices</p>
-    </div>
+    </button>
 
-    <div class="inventory_tab">
+    <button class="inventory_tab">
         <img src="Inventory Logo.png" alt="Inventory" style="width:50px;height:50px">
         <p>Inventory</p>
-    </div>
+    </button>
 
-    <div class="pos_tab">
+    <button class="pos_tab">
         <img src="POS LOgo.png" alt="Point of sales" style="width:50px;height:50px">
         <p>POS</p>
-    </div>
+    </button>
     
-    <div class="calender_tab">
+    <button class="calender_tab">
         <img src="Caldender Logo.png" alt="Calender" style="width:50px;height:50px">
         <p>Calender</p>
-    </div>
+    </button>
 
-    <div class="reports_tab">
+    <button class="reports_tab">
         <img src="Reports Logo.png" alt="Reports" style="width:50px;height:50px">
         <p>Reports</p>
-    </div>
+    </button>
 
-    <div class="delivery_tab">
+    <button class="delivery_tab">
         <img src="Delivery Logo.png" alt="Deliveries" style="width:50px;height:50px">
         <p>Delivery</p>
-    </div>
+    </button>
 
-    <div class="product_tab">
+    <button class="product_tab">
         <img src="18 1.png" alt="Products" style="width:50px;height:50px">
         <p>Prroducts</p>
-    </div>
+    </button>
 
-    <div class="admin_tab">
+    <button class="admin_tab">
         <img src="Admin Logo.png" alt="Administration" style="width:50px;height:50px">
         <p>Admin</p>
-    </div>
+    </button>
 
-    <div class="more_tab">
+    <button class="more_tab">
         <img src="More Logo.png" alt="More" style="width:50px;height:50px">
         <p>More</p>
-    </div>
+    </button>
 
     <div class="mia-s-sw_logo">
         <img src="Mask Group.png" alt="Mia's Sandwich shop" style="height:100px;">
