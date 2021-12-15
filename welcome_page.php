619db81032996675c9fd3a413b4f7537e692afe4
softwarecomplexity <96056869+softwarecomplexity@users.noreply.github.com>
2021-12-15 15:24:40 +0000

    adding css

--- welcome_page.php
+++ welcome_page.php
@@ -1,7 +1,29 @@
-<head>
-    <link rel="stylesheet" href="welcome_page.css">
-</head>
-<body>
-    <div class="heading">
-        <h1>Ay-up</h1>
-    </div>
+<html lang="en">
+          <head>
+            <meta charset="utf-8">
+          
+            <title>Html Generated</title>
+            <meta name="description" content="Figma htmlGenerator">
+            <meta name="author" content="htmlGenerator">
+            <link href="https://softwarecomplexity.tech/wp-admin/theme-editor.php?file=style.css&theme=intentionally-blank" rel="stylesheet">
+
+            <link rel="stylesheet" href="welcome_page.css">
+            
+            <style>
+              /*
+                Figma Background for illustrative/preview purposes only.
+                You can remove this style tag with no consequence
+              */
+              body {background: #E5E5E5; }
+            </style>
+          
+          </head>
+          
+          <body>
+            <div class=e111_1451><span  class="e111_1452">Welcome!</span><div  class="e111_1453"></div><span  class="e111_1454">© Strode 2021 Digital </span><div class=e111_1455><div  class="e111_1456"></div><span  class="e111_1457">Get Started</span><div  class="e111_1458"></div><div  class="e111_1459"></div><div  class="e111_1460"></div><div  class="e111_1461"></div><div  class="e111_1462"></div><div  class="e111_1463"></div><div  class="e111_1464"></div><div  class="e111_1465"></div><div  class="e111_1466"></div><div  class="e111_1467"></div><div  class="e111_1468"></div><div  class="e111_1469"></div><div  class="e111_1470"></div><div  class="e111_1471"></div><div  class="e111_1472"></div><span  class="e111_1473">New Customer</span><span  class="e111_1474">New Product</span><span  class="e111_1475">New Invoice</span><span  class="e111_1476">New Order</span><div  class="e111_1477"></div></div><div class=e111_1478><div  class="e111_1479"></div><div  class="e111_1480"></div><div  class="e111_1481"></div><div  class="e111_1482"></div><span  class="e111_1483">Reminders
+</span><span  class="e111_1484">Due At</span><span  class="e111_1485">Wed 09-12-21 05:52 AM</span><span  class="e111_1486">Wed 09-12-21 05:52 AM</span><span  class="e111_1487">Message</span><span  class="e111_1488">Status</span><span  class="e111_1489">For Tech</span><div  class="e111_1490"></div><div  class="e111_1491"></div><div  class="e111_1492"></div><div  class="e111_1493"></div><div  class="e111_1494"></div><div  class="e111_1495"></div><div  class="e111_1496"></div></div><div class=e111_1497><div  class="e111_1498"></div><div  class="e111_1499"></div><div  class="e111_1500"></div><div  class="e111_1501"></div><div  class="e111_1502"></div><div  class="e111_1503"></div><span  class="e111_1504">What’s New?</span><span  class="e111_1505">Wed 09-12-21 05:52 AM</span><span  class="e111_1506">Developer Update</span><span  class="e111_1507">New Order #5692</span><span  class="e111_1508">New Order #5391</span><span  class="e111_1509">Wed 09-12-21 05:52 AM</span><span  class="e111_1510">Wed 09-12-21 05:52 AM</span></div><div class=e111_1511><div  class="e111_1512"></div><span  class="e111_1513">Activity</span><span  class="e111_1514">More Reports</span><span  class="e111_1515">Orders Active: 1923
+Invoices: 9892
+Customer: 9203</span><span  class="e111_1516">Reports</span><span  class="e111_1517">Total Sales</span><div  class="e111_1518"></div><span  class="e111_1519">£4,905.98</span></div><div class=e111_1520><div  class="e111_1521"></div><span  class="e111_1522">ComplexityDesign</span><span  class="e111_1523">Search all things</span><span  class="e111_1524">Developer Mode</span><span  class="e111_1525">Nick Bosley</span><span  class="e111_1526">Help</span><div  class="e111_1527"></div><div  class="e111_1528"></div><div  class="e111_1529"></div><div  class="e111_1530"></div><div  class="e111_1531"></div><div  class="e111_1532"></div><div  class="e111_1533"></div><div  class="e111_1534"></div><div  class="e111_1535"></div><div  class="e111_1536"></div><div  class="e111_1537"></div><div  class="e111_1538"></div><div  class="e111_1539"></div><div  class="e111_1540"></div><div  class="e111_1541"></div><div  class="e111_1542"></div><div  class="e111_1543"></div><div  class="e111_1544"></div><span  class="e111_1545">Customers</span><span  class="e111_1546">Order</span><span  class="e111_1547">Invoices</span><span  class="e111_1548">Inventory</span><span  class="e111_1549">POS </span><span  class="e111_1550">Products</span><div class=e111_1551><span  class="e111_1552">Admin</span></div><div class=e111_1553><span  class="e111_1554">More</span></div><span  class="e111_1555">Calendar</span><span  class="e111_1556">Delivery</span><span  class="e111_1557">Reports
+</span><div  class="e111_1558"></div><div  class="e111_1559"></div><div  class="e111_1560"></div><div  class="e111_1561"></div><div  class="e111_1562"></div><div  class="e111_1563"></div><div  class="e111_1564"></div><div  class="e111_1565"></div><div  class="e111_1566"></div><div  class="e111_1567"></div><div  class="e111_1568"></div></div></div>
+          </body>
+          </html>
