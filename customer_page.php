69a607d27de95fabce3fde5d3d138a2cb62db040
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2021-12-15 13:07:48 +0000

    changing color of customer tab

--- customer_page.php
+++ customer_page.php
@@ -5,6 +5,11 @@
 <head>
     <link rel="stylesheet" href="customer_page.css">
     <link rel="stylesheet" href="universal.css">
+    <style>
+        .customer_tab {
+            background-color: #404040;
+        }
+    </style>
 </head>
 <body>
     <h1>In the jungle, the mighty jungle, the lion sleeps tonight</h1>
