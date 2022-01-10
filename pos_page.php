b96728f72e6761ceda6f67244dd0961356f4f2d9
softwarecomplexity <96056869+softwarecomplexity@users.noreply.github.com>
2022-01-10 14:14:12 +0000

    Idk

--- pos_page.php
+++ pos_page.php
@@ -11,3 +11,81 @@
         }
     </style>
 </head>
+
+<div id=e111_1569>
+  <div id=e111_1570>
+    <div id="e111_1571"></div>
+    <div id="e111_1572"></div><span  id="e111_1573">ComplexityDesign</span><span  id="e111_1574">Search all things</span><span  id="e111_1575">Developer Mode</span><span  id="e111_1576">Nick Bosley</span><span  id="e111_1577">Help</span>
+    <div id="e111_1578"></div>
+    <div id="e111_1579"></div>
+    <div id="e111_1580"></div>
+    <div id="e111_1581"></div>
+    <div id="e111_1582"></div>
+    <div id="e111_1583"></div>
+    <div id="e111_1584"></div>
+    <div id="e111_1585"></div>
+    <div id="e111_1586"></div>
+    <div id="e111_1587"></div>
+    <div id="e111_1588"></div>
+    <div id="e111_1589"></div>
+    <div id="e111_1590"></div>
+    <div id="e111_1591"></div>
+    <div id="e111_1592"></div>
+    <div id="e111_1593"></div>
+    <div id="e111_1594"></div>
+    <div id="e111_1595"></div>
+    <div id="e111_1596"></div><span  id="e111_1597">Customers</span><span  id="e111_1598">Order</span><span  id="e111_1599">Invoices</span><span  id="e111_1600">Inventory</span><span  id="e111_1601">POS </span><span  id="e111_1602">Products</span>
+    <div id=e111_1603><span  id="e111_1604">Admin</span></div>
+    <div id=e111_1605><span  id="e111_1606">More</span></div><span  id="e111_1607">Calendar</span><span  id="e111_1608">Delivery</span><span  id="e111_1609">Reports
+</span>
+    <div id="e111_1610"></div>
+    <div id="e111_1611"></div>
+    <div id="e111_1612"></div>
+    <div id="e111_1613"></div>
+    <div id="e111_1614"></div>
+    <div id="e111_1615"></div>
+    <div id="e111_1616"></div>
+    <div id="e111_1617"></div>
+    <div id="e111_1618"></div>
+  </div>
+  <div id="e111_1619"></div>
+  <div id="e111_1620"></div>
+  <div id=e111_1621>
+    <div id=e111_1623>
+      <div id="e111_1624"></div>
+      <div id="e111_1625"></div>
+    </div>
+  </div>
+  <div id=e111_1626>
+    <div id=e111_1628>
+      <div id="e111_1629"></div>
+      <div id="e111_1630"></div>
+    </div>
+  </div>
+  <div id=e111_1631>
+    <div id=e111_1633>
+      <div id="e111_1634"></div>
+      <div id="e111_1635"></div>
+      <div id="e174_7"></div><span  id="e174_27">Taxable</span><span  id="e174_34">Wholesale</span>
+      <div id="e174_3"></div>
+      <div id="e174_2"></div><span  id="e174_28">Add Item</span>
+      <div id="e174_9"></div>
+      <div id="e174_6"></div><span  id="e174_29">Add Manual Item</span>
+    </div><span  id="e174_26">Quantity</span><span  id="e174_31">Item</span><span  id="e174_23">Description</span>
+    <div id="e174_5"></div><span  id="e174_24">Price</span><span  id="e174_25">Cost</span>
+    <div id="e174_8"></div>
+  </div>
+  <div id="e174_14"></div>
+  <div id="e174_39"></div><span  id="e174_33">Quantity</span>
+  <div id="e174_10"></div>
+  <div id="e174_12"></div>
+  <div id="e174_13"></div>
+  <div id="e174_15"></div>
+  <div id="e174_16"></div>
+  <div id="e174_17"></div>
+  <div id="e174_19"></div>
+  <div id="e174_49"></div>
+  <div id="e174_50"></div><span  id="e174_32">Item</span><span  id="e174_35">Add Item</span><span  id="e174_30">Add From Inventory</span><span  id="e174_36">Ham Sandwich</span><span  id="e174_38">1.@</span><span  id="e174_41">£0.00</span><span  id="e174_42">Subtotal:</span><span  id="e174_43">Tax:</span><span  id="e174_44">Total:</span><span  id="e174_45">£xxx.xx</span><span  id="e174_46">£xxx.xx</span><span  id="e174_47">£xxx.xx</span><span  id="e174_51">Cash</span><span  id="e174_53">Paypal</span><span  id="e174_54">Credit</span>
+  <div id="e174_55"></div>
+  <div id="e174_56"></div>
+</div>
