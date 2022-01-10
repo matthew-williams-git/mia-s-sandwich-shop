61e01268fc3592621b28d0b809e39e1bb9b670bc
softwarecomplexity <96056869+softwarecomplexity@users.noreply.github.com>
2022-01-10 09:44:06 +0000

    Reports Feature

--- welcome_page.php
+++ welcome_page.php
@@ -47,3 +47,69 @@
     ❆
   </div>
 </div>
+
+/* Text */
+
+<span  class="e111_1454">© Strode 2021 Digital </span>
+<div class="e111_1453"></div>
+<span  class="e111_1452">Welcome!</span>
+
+/* Get Started */
+
+<div class=e111_1455>
+  <div class="e111_1456"></div><span  class="e111_1457">Get Started</span>
+  <div class="e111_1458"></div>
+  <div class="e111_1459"></div>
+  <div class="e111_1460"></div>
+  <div class="e111_1461"></div>
+  <div class="e111_1462"></div>
+  <div class="e111_1463"></div>
+  <div class="e111_1464"></div>
+  <div class="e111_1465"></div>
+  <div class="e111_1466"></div>
+  <div class="e111_1467"></div>
+  <div class="e111_1468"></div>
+  <div class="e111_1469"></div>
+  <div class="e111_1470"></div>
+  <div class="e111_1471"></div>
+  <div class="e111_1472"></div><span  class="e111_1473">New Customer</span><span  class="e111_1474">New Product</span><span  class="e111_1475">New Invoice</span><span  class="e111_1476">New Order</span>
+  <div class="e111_1477"></div>
+</div>
+
+
+/* Reminders */ 
+
+<div class=e111_1478>
+  <div class="e111_1479"></div>
+  <div class="e111_1480"></div>
+  <div class="e111_1481"></div>
+  <div class="e111_1482"></div><span  class="e111_1483">Reminders
+</span><span  class="e111_1484">Due At</span><span  class="e111_1485">Wed 09-12-21 05:52 AM</span><span  class="e111_1486">Wed 09-12-21 05:52 AM</span><span  class="e111_1487">Message</span><span  class="e111_1488">Status</span><span  class="e111_1489">For Tech</span>
+  <div class="e111_1490"></div>
+  <div class="e111_1491"></div>
+  <div class="e111_1492"></div>
+  <div class="e111_1493"></div>
+  <div class="e111_1494"></div>
+  <div class="e111_1495"></div>
+  <div class="e111_1496"></div>
+</div>
+
+/* What's New * /
+
+<div class=e111_1497>
+  <div class="e111_1498"></div>
+  <div class="e111_1499"></div>
+  <div class="e111_1500"></div>
+  <div class="e111_1501"></div>
+  <div class="e111_1502"></div>
+  <div class="e111_1503"></div><span  class="e111_1504">What’s New?</span><span  class="e111_1505">Wed 09-12-21 05:52 AM</span><span  class="e111_1506">Developer Update</span><span  class="e111_1507">New Order #5692</span><span  class="e111_1508">New Order #5391</span><span  class="e111_1509">Wed 09-12-21 05:52 AM</span><span  class="e111_1510">Wed 09-12-21 05:52 AM</span>
+</div>
+
+/* Reports */
+
+<div class=e111_1511>
+  <div class="e111_1512"></div><span  class="e111_1513">Activity</span><span  class="e111_1514">More Reports</span><span  class="e111_1515">Orders Active: 1923
+Invoices: 9892
+Customer: 9203</span><span  class="e111_1516">Reports</span><span  class="e111_1517">Total Sales</span>
+  <div class="e111_1518"></div><span  class="e111_1519">£4,905.98</span>
+</div>
