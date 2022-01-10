26cfc8aafdda0a35c4c5aa20b58f731da89faa91
softwarecomplexity <96056869+softwarecomplexity@users.noreply.github.com>
2022-01-10 10:57:58 +0000

    Pushing More

--- welcome_page.php
+++ welcome_page.php
@@ -1,111 +1,19 @@
-<style>
-/* customizable snowflake styling */
-.snowflake {
-  color: #fff;
-  font-size: 1em;
-  font-family: Arial, sans-serif;
-  text-shadow: 0 0 5px #000;
-}
-
-@-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%,100%{-webkit-transform:translateX(0);transform:translateX(0)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}.snowflake:nth-of-type(10){left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s}.snowflake:nth-of-type(11){left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s}
-</style>
-<div class="snowflakes" aria-hidden="true">
-  <div class="snowflake">
-  ❅
-  </div>
-  <div class="snowflake">
-  ❆
-  </div>
-  <div class="snowflake">
-  ❅
-  </div>
-  <div class="snowflake">
-  ❆
-  </div>
-  <div class="snowflake">
-  ❅
-  </div>
-  <div class="snowflake">
-  ❆
-  </div>
-  <div class="snowflake">
-    ❅
-  </div>
-  <div class="snowflake">
-    ❆
-  </div>
-  <div class="snowflake">
-    ❅
-  </div>
-  <div class="snowflake">
-    ❆
-  </div>
-  <div class="snowflake">
-    ❅
-  </div>
-  <div class="snowflake">
-    ❆
-  </div>
-</div>
-
-
-
-<span  class="e111_1454">© Strode 2021 Digital </span>
-<div class="e111_1453"></div>
-<span  class="e111_1452">Welcome!</span>
-
-
-
-<div id=e111_1497>
-  <div id="e111_1498"></div>
-  <div id="e111_1499"></div>
-  <div id="e111_1500"></div>
-  <div id="e111_1501"></div>
-  <div id="e111_1502"></div>
-  <div id="e111_1503"></div><span  id="e111_1504">What’s New?</span><span  id="e111_1505">Wed 09-12-21 05:52 AM</span><span  id="e111_1506">Developer Update</span><span  id="e111_1507">New Order #5692</span><span  id="e111_1508">New Order #5391</span><span  id="e111_1509">Wed 09-12-21 05:52 AM</span><span  id="e111_1510">Wed 09-12-21 05:52 AM</span>
-</div>
-
-
-
-<div class=e111_1455>
-  <div class="e111_1456"></div><span  class="e111_1457">Get Started</span>
-  <div class="e111_1458"></div>
-  <div class="e111_1459"></div>
-  <div class="e111_1460"></div>
-  <div class="e111_1461"></div>
-  <div class="e111_1462"></div>
-  <div class="e111_1463"></div>
-  <div class="e111_1464"></div>
-  <div class="e111_1465"></div>
-  <div class="e111_1466"></div>
-  <div class="e111_1467"></div>
-  <div class="e111_1468"></div>
-  <div class="e111_1469"></div>
-  <div class="e111_1470"></div>
-  <div class="e111_1471"></div>
-  <div class="e111_1472"></div><span  class="e111_1473">New Customer</span><span  class="e111_1474">New Product</span><span  class="e111_1475">New Invoice</span><span  class="e111_1476">New Order</span>
-  <div class="e111_1477"></div>
-</div>
-Chang
-
-<div class=e111_1478>
-  <div class="e111_1479"></div>
-  <div class="e111_1480"></div>
-  <div class="e111_1481"></div>
-  <div class="e111_1482"></div><span  class="e111_1483">Reminders
-</span><span  class="e111_1484">Due At</span><span  class="e111_1485">Wed 09-12-21 05:52 AM</span><span  class="e111_1486">Wed 09-12-21 05:52 AM</span><span  class="e111_1487">Message</span><span  class="e111_1488">Status</span><span  class="e111_1489">For Tech</span>
-  <div class="e111_1490"></div>
-  <div class="e111_1491"></div>
-  <div class="e111_1492"></div>
-  <div class="e111_1493"></div>
-  <div class="e111_1494"></div>
-  <div class="e111_1495"></div>
-  <div class="e111_1496"></div>
-</div>
-
-<div class=e111_1511>
-  <div class="e111_1512"></div><span  class="e111_1513">Activity</span><span  class="e111_1514">More Reports</span><span  class="e111_1515">Orders Active: 1923
-Invoices: 9892
-Customer: 9203</span><span  class="e111_1516">Reports</span><span  class="e111_1517">Total Sales</span>
-  <div class="e111_1518"></div><span  class="e111_1519">£4,905.98</span>
+<div id=e111_1455>
+  <div id="e111_1456"></div><span  id="e111_1457">Get Started</span>
+  <div id="e111_1458"></div>
+  <div id="e111_1459"></div>
+  <div id="e111_1460"></div>
+  <div id="e111_1461"></div>
+  <div id="e111_1462"></div>
+  <div id="e111_1463"></div>
+  <div id="e111_1464"></div>
+  <div id="e111_1465"></div>
+  <div id="e111_1466"></div>
+  <div id="e111_1467"></div>
+  <div id="e111_1468"></div>
+  <div id="e111_1469"></div>
+  <div id="e111_1470"></div>
+  <div id="e111_1471"></div>
+  <div id="e111_1472"></div><span  id="e111_1473">New Customer</span><span  id="e111_1474">New Product</span><span  id="e111_1475">New Invoice</span><span  id="e111_1476">New Order</span>
+  <div id="e111_1477"></div>
 </div>
