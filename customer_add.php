e847b4772fb72692add1e41a65a95853bd5b4bc9
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-11 14:03:59 +0000

    shnazzle

---
+++ customer_add.php
@@ -0,0 +1,22 @@
+<?php
+    include('top_bar.php');
+    include('ribbon.php');
+?>
+<head>
+    <link rel="stylesheet" href="customer_add.css">
+    <link rel="stylesheet" href="universal.css">
+    <style>
+        .customer_tab {
+            background-color: #404040;
+        }
+    </style>
+</head>
+<body>
+    <div class="page_title">
+        <h1>New Customer</h1>
+    </div>
+
+    <div class="background">
+        
+    </div>
+</body>
