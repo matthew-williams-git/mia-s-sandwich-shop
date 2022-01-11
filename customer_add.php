7e419da28f773f08f3979b700beff0f3bbcd8aca
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-11 15:49:49 +0000

    adding buttons to cancel and proceed

--- customer_add.php
+++ customer_add.php
@@ -19,4 +19,18 @@
     <!-- load of shit, will remove later<div class="background">
         
     </div> -->
+    
+    <a href="customer_page.php">
+        <div class="confirm_btn">
+            <img src="title_bar_images/Plus Icon.png">
+            <p>Confirm</p>
+        </div>
+    </a>
+    
+    <a href="customer_page.php">
+        <div class="cancel_btn">
+            <img src="title_bar_images/Plus Icon.png">
+            <p>Cancel</p>
+        </div>
+    </a>
 </body>
