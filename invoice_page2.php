d31c0655bc17f339af957a84b639b152b4f48092
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-11 12:15:12 +0000

    adding another input table

--- invoice_page2.php
+++ invoice_page2.php
@@ -36,24 +36,46 @@
         <legend>Enter Cusomer/Business details</legend><br><br>
 
         <label for="bname">Business or Customer name</label>
-        <input type="text" id="bname" name="bname" value="Business name"><br><br>
+        <input type="text" id="bname" name="bname_input" value="Business name"><br><br>
 
         <label for="acontact">Assigned Contact</label>
-        <input type="text" id="acontact" name="acontact" value="Assigned Contact"><br><br>
+        <input type="text" id="acontact" name="acontact_input" value="Assigned Contact"><br><br>
 
         <label for="title">Title</label>
-        <input type="text" id="title" name="title" value="Mr, Mrs, Dr, Ms"><br><br>
+        <input type="text" id="title" name="title_input" value="Mr, Mrs, Dr, Ms"><br><br>
 
         <label for="emailaddress">Email Address</label>
-        <input type="text" id="emailaddress" name="emailaddress" value="nobodyknows@whocares.co.uk"><br><br>
+        <input type="email" id="emailaddress" name="emailaddress_input" value="nobodyknows@whocares.co.uk"><br><br>
 
         <label for="pnumber">Phone Number</label>
-        <input type="text" id="pname2" name="pname"><br><br>
+        <input type="text" id="pname2" name="pname_input"><br><br>
 
         <label for="postaladdress">Postal/delivery address</label>
-        <textarea name="postaladdress" id="postaladdress" cols="30" rows="10">lollypop lollypop, oh lolly lolly lolly lollypop.... ubub bumb bumb</textarea><br><br>
+        <textarea name="postaladdress_input" id="postaladdress" cols="30" rows="10">lollypop lollypop, oh lolly lolly lolly lollypop.... ubub bumb bumb</textarea><br><br>
 
         <label for="extrainfo">Additional Information</label>
-        <textarea name="extrainfo" id="extrainfo" cols="30" rows="10"></textarea>
+        <textarea name="extrainfo_input" id="extrainfo" cols="30" rows="10"></textarea>
+    </div>
+
+    <div class="invoice_details">
+        <legend>Invoice details</legend><br><br>
+
+        <label for="invoice_number"> Invoice Number</label>
+        <input type="text" id="invoice_number" name="invoice_number_input"><br><br>
+
+        <label for="status">Status</label>
+        <input type="text" id="status" name="status_input" value="in limbo"><br><br>
+
+        <label for="invoicedate">Invoicing date</label>
+        <input type="date" id="invoicedate" name="invoicedate_input" value="current date"><br><br> <!-- work out how to get current date -->
+
+        <label for="taxdate">Tax date</label>
+        <input type="date" id="taxdate" name="taxdate_input"><br><br> <!-- work out how to display date one month from now -->
+
+        <label for="paymentdate">Payment Due:</label>
+        <input type="date" id="paymentdate" name="paymentdate_input"><br><br>
+
+        <label for="amountdue">Amount Due</label>
+        <input type="number" id="amountdue" name="amountdue_input" value="way to much">
     </div>
 </body>
