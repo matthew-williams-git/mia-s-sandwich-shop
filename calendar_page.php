c34fc12d00e54c5aaaf33f2ffb21f4269b7b1ec8
Ffreddie64 <81242081+Ffreddie64@users.noreply.github.com>
2022-01-11 16:10:23 +0000

    Update calendar_page.php

--- calendar_page.php
+++ calendar_page.php
@@ -13,6 +13,19 @@
 </head>
 <body>
     <div class="add-calendar">
-    
+        <h1>Add To Calendar</h1>
+        
+        <label for="eventname">Name Of Event</label><br>
+        <input type="text" id="eventname" name="eventname_input"><br><br>
+            
+        <label for="datetime">Time And Date</label><br>
+        <input type="datetime" id="datetime" name="datetime_input"><br><br>
+        
+        <label for="eventlocation">Location</label><br>
+        <input type="text" id="eventlocation" name="eventlocation_input"><br><br>
+    </div>
+     
+    <div class="calendar-main">
+        
     </div>
 </body>
