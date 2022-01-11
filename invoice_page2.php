b7fc09e6cb52d0fd67d6e20e551d774d1a5b922e
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-11 09:36:18 +0000

    adding buttons & css

--- invoice_page2.php
+++ invoice_page2.php
@@ -12,5 +12,22 @@
     </style>
 </head>
 <body>
-    <h1>it works</h1>
+    <div class="page_head">
+        <h1>New Invoice</h1>
+    </div>
+
+    <a href="invoice_page1.php">
+        <div class="cancel_button">
+            <img src="title_bar_images/Plus Icon.png">
+            <p>Cancel</p>
+        </div>
+    </a>
+
+    <!-- add php in here somewhere to input data into the database -->
+    <a href="invoice_page1.php">
+        <div class="confirm_button">
+            <img src="title_bar_images/Plus Icon.png">
+            <p>Confirm</p>
+        </div>
+    </a>
 </body>
