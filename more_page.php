5e93ded0e35a0e234ece290a83d23ffd2c557197
softwarecomplexity <96056869+softwarecomplexity@users.noreply.github.com>
2022-01-12 11:35:13 +0000

    Everything

---
+++ more_page.php
@@ -0,0 +1,13 @@
+<?php
+    include('top_bar.php');
+    include('ribbon.php');
+?>
+<head>
+    <link rel="stylesheet" href="more_page.css">
+    <link rel="stylesheet" href="universal.css">
+    <style>
+        .more_tab {
+            background-color: #404040;
+        }
+    </style>
+</head>
