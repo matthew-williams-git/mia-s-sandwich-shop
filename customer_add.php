64faeb4c94bcbeaa7eecff606d06128fffec3179
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-12 14:27:02 +0000

    adding customer details and phone number fields

--- customer_add.php
+++ customer_add.php
@@ -16,7 +16,7 @@
     <div class="page_title">
         <h1>New Customer</h1>
     </div>
-    
+    <!-- buttons, top right of page -->
     <a href="customer_page.php">
         <div class="confirm_button">
             <img src="title_bar_images/Plus Icon.png">
@@ -31,5 +31,41 @@
         </div>
     </a>
     
+    <!-- left hand form -->
+    <div class="Customer_details">
+        <div class="left_culumn">
+            <label for="customer_fname">First name</label><br>
+            <input type="text" id="cusotmer_fname" name="customer_fname" placeholder="Enter first name..."><br>
+
+            <label for="customer_title">Title</label><br>
+            <select name="customer_title" id="title">
+                <option value="Mr">Mr</option>
+                <option value="Mrs">Mrs</option>
+                <option value="Ms">Ms</option>
+                <option value="Mx">Mx</option>
+            </select>
+        </div>
+
+        <div class="right_culumn">
+            <label for="customer_lname">Last Name</label>
+            <input type="text" id="customer_lname" name="customer_lname" placeholder="Enter last name...">
+        </div>
+
+        <div class="phone_number">
+            <h3>Contact details</h3>
+            <label for="customer_pnumber">Phone number</label><br>
+            <input type="text" id="customer_pnumber" name="customer_pnumber" placeholder="07792329851">
+        </div>
+
+        <div class="phone_type">
+            <h3></h3>
+            <label for="phone_type">Phone type</label>
+            <select name="phone_type" id="phone">
+                <option value="Mobile">Mobile</option>
+                <option value="Home_landline">Home landline</option>
+                <option value="Office_landline">Office landline</option>
+            </select>
+        </div>
+    </div>
 
 </body>
