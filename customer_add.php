ddb39216e16d6ebe208a66a8103556af75b829e1
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-12 09:11:55 +0000

    changing html order to fix position

--- customer_add.php
+++ customer_add.php
@@ -17,16 +17,18 @@
     </div>
     
     <a href="customer_page.php">
+        <div class="cancel_button">
+            <img src="title_bar_images/Plus Icon.png">
+            <p>Cancel</p>
+        </div>
+    </a>    
+    
+    <a href="customer_page.php">
         <div class="confirm_button">
             <img src="title_bar_images/Plus Icon.png">
             <p>Confirm</p>
         </div>
     </a>
     
-    <a href="customer_page.php">
-        <div class="cancel_button">
-            <img src="title_bar_images/Plus Icon.png">
-            <p>Cancel</p>
-        </div>
-    </a>
+
 </body>
