1615daad6cebd6d31d1c076a00d658f8d83c5fbc
Ffreddie64 <81242081+Ffreddie64@users.noreply.github.com>
2022-01-14 13:28:20 +0000

    Update calendar_page.php

--- calendar_page.php
+++ calendar_page.php
@@ -35,10 +35,105 @@
     </form>
      
     <div class="calendar-main">
-        <h1>Calendar</h1>
+        <div class="month-indicator"> ... </div>
+        <div class="day-of-week">
+            <div>Su</div>
+            <div>Mo</div>
+            <div>Tu</div>
+            <div>We</div>
+            <div>Th</div>
+            <div>Fr</div>
+            <div>Sa</div>
+        </div>
         
-        <button type="addeventbutton">New Event</button>
+        /* Credit to zellwk.com 2019 for the calendar grid */
         
-        
+        <div class="date-grid">
+            <button>
+                <time datetime="2022-02-01">1</time>
+            </button>
+            <button>
+                <time datetime="2022-02-02">2</time>
+            </button>
+            <button>
+                <time datetime="2022-02-03">3</time>
+            </button>
+            <button>
+                <time datetime="2022-02-04">4</time>
+            </button>
+            <button>
+                <time datetime="2022-02-05">5</time>
+            </button>
+            <button>
+                <time datetime="2022-02-06">6</time>
+            </button>
+            <button>
+                <time datetime="2022-02-07">7</time>
+            </button>
+            <button>
+                <time datetime="2022-02-08">8</time>
+            </button>
+            <button>
+                <time datetime="2022-02-09">9</time>
+            </button>
+            <button>
+                <time datetime="2022-02-10">10</time>
+            </button>
+            <button>
+                <time datetime="2022-02-11">11</time>
+            </button>
+            <button>
+                <time datetime="2022-02-12">12</time>
+            </button>
+            <button>
+                <time datetime="2022-02-13">13</time>
+            </button>
+            <button>
+                <time datetime="2022-02-14">14</time>
+            </button>
+            <button>
+                <time datetime="2022-02-15">15</time>
+            </button>
+            <button>
+                <time datetime="2022-02-16">16</time>
+            </button>
+            <button>
+                <time datetime="2022-02-17">17</time>
+            </button>
+            <button>
+                <time datetime="2022-02-18">18</time>
+            </button>
+            <button>
+                <time datetime="2022-02-19">19</time>
+            </button>
+            <button>
+                <time datetime="2022-02-20">20</time>
+            </button>
+            <button>
+                <time datetime="2022-02-21">21</time>
+            </button>
+            <button>
+                <time datetime="2022-02-22">22</time>
+            </button>
+            <button>
+                <time datetime="2022-02-23">23</time>
+            </button>
+            <button>
+                <time datetime="2022-02-24">24</time>
+            </button>
+            <button>
+                <time datetime="2022-02-25">25</time>
+            </button>
+            <button>
+                <time datetime="2022-02-26">26</time>
+            </button>
+            <button>
+                <time datetime="2022-02-27">27</time>
+            </button>
+            <button>
+                <time datetime="2022-02-28">28</time>
+            </button>
+
+        </div>      
     </div>
 </body>
