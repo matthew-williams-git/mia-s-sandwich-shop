5672537416cc692d6400ecb3cf9708ad32bf973c
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2022-01-17 13:22:17 +0000

    Adding attributes for admin login

--- admin_page1.php
+++ admin_page1.php
@@ -6,9 +6,19 @@
 <head>
     <link rel="stylesheet" href="admin_page1.css">
     <link rel="stylesheet" href="universal.css">
-    <style>
-        .admin_tab {
-            background-color: #404040;
-        }
-    </style>
 </head>
+
+<body>
+    <div class="admin-interface">
+        <form action="">
+            <label id="title">Admin Login</label><br>
+            <label id="command">Enter Details</label><br>
+
+            <input type="text"   name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number"><br><br>
+            <input type="text"   name="uname"    id="uname" placeholder="Username"><br><br>
+            <input type="text"   name="pword"    id="pword" placeholder="Password"><br><br>
+            <input type="submit" name="submit"   id="submit" value="Submit">
+
+        </form>
+    </div>
+</body>
