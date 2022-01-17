eb2602b31a3b6f263629cc43c12c58102b846c1c
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-17 09:21:46 +0000

    removing nicks pp from index.php

--- index.php
+++ index.php
@@ -22,36 +22,4 @@
 </html>
 
 <!--- here is the SQL injection prevention that links with the DB --->
-
-<?php
-$id = $_GET['id'];
-$db_connection = new PDO('mysql:host=localhost;dbname=sql_injection_example', 'dbuser', 'dbpasswd');
-//preparing the query
-$sql = "SELECT firstname
-            FROM users
-            WHERE id = :id";
-$query = $db_connection->prepare($sql);
-$query->bindParam(':id', $id);
-$query->execute();
-//getting the result
-$query->setFetchMode(PDO::FETCH_ASSOC);
-$result = $query->fetchColumn();
-print(htmlentities($result));
-?>
-
-<?php
-if (isset($_POST["selRating"]))
-{
-    $number = $_POST["selRating"];
-    if((is_numeric($number)) && ($number > 0) && ($number > 6)
-}
-echo "Selected Rating:" . $number;
-
-else 
-    echo  "The rating has to be a number a 1 and 2"
-
-}
-?>
-
-<?php $database = new SQLite3(Mias SSDB.db)
-?>
+<!-- in nicks stuff txt -->
