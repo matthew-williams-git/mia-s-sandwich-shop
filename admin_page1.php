88da81062a39866dcbc7329938cd0e1c92a47ee3
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2022-01-17 14:04:31 +0000

    adding flavour to login

--- admin_page1.php
+++ admin_page1.php
@@ -14,9 +14,9 @@
             <label id="title">Admin Login</label><br>
             <label id="command">Enter Details</label><br>
 
-            <input type="text"   name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br>
-            <input type="text"   name="uname"    id="uname" placeholder="Username"><br><br>
-            <input type="text"   name="pword"    id="pword" placeholder="Password"><br><br>
+            <input class="inputs" type="text"   name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br>
+            <input class="inputs" type="text"   name="uname"    id="uname" placeholder="Username"><br><br>
+            <input class="inputs" type="text"   name="pword"    id="pword" placeholder="Password"><br><br>
             <input type="submit" name="submit"   id="submit" value="Submit">
 
         </form>
