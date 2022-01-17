cc874d7bed2e0649b455dd1254cc17e0f878506d
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2022-01-17 14:17:29 +0000

    tweaks 1

--- admin_page1.php
+++ admin_page1.php
@@ -12,12 +12,12 @@
     <div class="admin-interface">
         <form action="#" method="post">
             <label id="title">Admin Login</label><br>
-            <label id="command">Enter Details</label><br>
+            <label id="command">Enter Details</label><br><br>
 
-            <input class="inputs" type="text"   name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br>
-            <input class="inputs" type="text"   name="uname"    id="uname" placeholder="Username"><br><br>
-            <input class="inputs" type="text"   name="pword"    id="pword" placeholder="Password"><br><br>
-            <input type="submit" name="submit"   id="submit" value="Submit">
+            <input class="inputs" type="text"       name="branchid" id="branchid"   pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br>
+            <input class="inputs" type="text"       name="uname"    id="uname"      placeholder="Username"><br><br>
+            <input class="inputs" type="password"   name="pword"    id="pword"      placeholder="Password"><br><br>
+            <input type="submit"  name="submit"     id="submit"     value="Submit">
 
         </form>
     </div>
