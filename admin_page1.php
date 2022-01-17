ea8b7b71ea6d1c193e69488ce60aa9c7feaa1933
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2022-01-17 14:40:53 +0000

    tweaks 3

--- admin_page1.php
+++ admin_page1.php
@@ -14,10 +14,10 @@
             <label class="title">Admin Login</label><br>
             <label class="command">Enter Details</label><br><br>
 
-            <input class="inputs" type="text"       name="branchid" id="branchid"   pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br>
-            <input class="inputs" type="text"       name="uname"    id="uname"      placeholder="Username"><br><br>
-            <input class="inputs" type="password"   name="pword"    id="pword"      placeholder="Password"><br><br>
-            <input type="submit"  name="submit"     id="submit"     value="Submit">
+            <input class="inputs" type="text"     name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br><br>
+            <input class="inputs" type="text"     name="uname"    id="uname"    placeholder="Username"><br><br><br>
+            <input class="inputs" type="password" name="pword"    id="pword"    placeholder="Password"><br><br><br>
+            <input class="submit" type="submit"   name="submit"   id="submit"   value="Submit">
 
         </form>
     </div>
