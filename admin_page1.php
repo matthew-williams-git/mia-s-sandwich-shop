5f0f799ecaad2ca43686b29fbf57a0fe7c64cce7
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2022-01-17 15:23:34 +0000

    added interface and verification pages

--- admin_page1.php
+++ admin_page1.php
@@ -10,13 +10,13 @@
 
 <body>
     <div class="admin-interface">
-        <form action="#" method="post">
+        <form action="admin_ver.php" method="post">
             <label class="title">Admin Login</label><br>
             <label class="command">Enter Details</label><br><br>
 
-            <input class="inputs" type="text"     name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br><br>
-            <input class="inputs" type="text"     name="uname"    id="uname"    placeholder="Username"><br><br><br>
-            <input class="inputs" type="password" name="pword"    id="pword"    placeholder="Password"><br><br><br>
+            <input class="inputs" type="text"     name="branchid" id="branchid" pattern="[0-9]{3}" title="Branch Number" placeholder="Branch Number"><br><br>
+            <input class="inputs" type="text"     name="uname"    id="uname"    placeholder="Username"><br><br>
+            <input class="inputs" type="password" name="pword"    id="pword"    placeholder="Password"><br><br>
             <input class="submit" type="submit"   name="submit"   id="submit"   value="Submit">
 
         </form>
