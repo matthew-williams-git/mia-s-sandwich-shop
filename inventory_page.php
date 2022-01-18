0a5f921926ecc9f37db875e33d90fbf190585969
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-18 08:49:36 -0500

    final changes to table size

--- inventory_page.php
+++ inventory_page.php
@@ -54,19 +54,29 @@
         <div class="inventory_table">
             <table>
                 <tbody><tr>
-                    <th style="width: 50px;">     </th>
+                    <th style="width: 90px;">     </th>
                     <th style="width: 250px;">Name</th>
                     <th style="width: 600px">Description</th>
                     <th style="width: 100px;">Cost</th>
                     <th style="width: 100px;">Retail</th>
-                    <th style="width: 100;">Quantity</th>
+                    <th style="width: 100px;">Quantity</th>
                     <th style="width: 100px;">Catagory</th>
-                    <th style="width: 50px;">     </th>
+                    <th style="width: 130px;">     </th>
                 </tr>
                 <tr>
                     <td>     </td>
                     <td>White Bread</td>
                     <td>White sandwich bread</td>
+                    <td>£xx.xx</td>
+                    <td>£xx.xx</td>
+                    <td>xx</td>
+                    <td>Bread</td>
+                    <td>     </td>
+                </tr>
+                <tr>
+                    <td>     </td>
+                    <td>Brown Bread</td>
+                    <td>Brown bread for sandwich making</td>
                     <td>£xx.xx</td>
                     <td>£xx.xx</td>
                     <td>xx</td>
