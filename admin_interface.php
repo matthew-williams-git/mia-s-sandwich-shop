cb92bd0f82a5587fbdab04e46e4bfba7dc08b874
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-18 10:58:40 +0000

    fixing yet more blunders and probably creating a few more

--- admin_interface.php
+++ admin_interface.php
@@ -1,3 +1,14 @@
 <?php
-session_start();
+    session_start();
+    include('top_bar.php');
+    include('ribbon.php');
 ?>
+<head>
+    <link rel="stylesheet" href="admin_interface.css">
+    <link rel="stylesheet" href="universal.css">
+    <style>
+        .admin_tab {
+            background-color: #404040;
+        }
+    </style>
+</head>
--- admin_page1.php
+++ admin_page1.php
@@ -6,6 +6,11 @@
 <head>
     <link rel="stylesheet" href="admin_page1.css">
     <link rel="stylesheet" href="universal.css">
+    <style>
+        .admin_tab {
+            background-color: #404040;
+        }
+    </style>
 </head>
 
 <body>
