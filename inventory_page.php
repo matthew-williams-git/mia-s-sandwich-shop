96482a0744dcedb2a0e73693f259238be4e35bfb
matthew-williams-git <81242149+matthew-williams-git@users.noreply.github.com>
2022-01-18 11:38:39 +0000

    starting inventory page

--- inventory_page.php
+++ inventory_page.php
@@ -12,3 +12,20 @@
         }
     </style>
 </head>
+<body>
+    <div class="page_title">
+        <h1>Inventory</h1>
+    </div>
+
+    <a href="invoice_page2.php">
+        <div class="add_item_btn">
+            <img src="title_bar_images/Plus Icon.png">
+            <p>New invoice</p>
+        </div>
+    </a>
+
+    <div class="search_bar">
+        <input type="search" name="search_bar" id="search_bar">
+        <input type="submit" value="Go!">
+    </div>
+</body>
